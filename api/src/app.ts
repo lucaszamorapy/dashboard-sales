cbdc54778b4dcdf5f26d0680d65522fcd3891907
lucaszamorapy <lucaszamora.zulmira@gmail.com>
2025-03-15 13:23:36 -0300

    chore: initialization

---
+++ api/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes';
+import { sequelizeDb } from './config';
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use("/api", router)
+const port = 8080
+
+const start = async () => {
+  try {
+    await sequelizeDb.authenticate()
+    console.log('Conexão ao banco de dados estabelecida')
+    app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`)
+    })
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+    setTimeout(start, 5000);
+  }
+}
+start();
---
+++ api/src/config/index.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const sequelizeDb = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
